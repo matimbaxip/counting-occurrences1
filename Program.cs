bfe6d6964e890ca0af657aab152cdba35517629a
Dell-User <Dell-User@DESKTOP-4LNHG9G>
2023-10-16 10:43:53 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace counting_occurrences1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(HowManyOccurrences("do it now", "do")); // 1
+            Console.WriteLine(HowManyOccurrences("empty", "d")); // 0
+        }
+
+        static int HowManyOccurrences(string input, string substring)
+        {
+            int count = 0;
+            int index = input.IndexOf(substring);
+
+            while (index != -1)
+            {
+                count++;
+                index = input.IndexOf(substring, index + 1);
+            }
+
+            return count;
+        }
+    }
+}
